4f7aa6dfe30b118edba299fa763d060defae755a
00msjr <michaelsousajr@gmail.com>
2025-09-29 21:06:35 -0400

    flags, help msg, list

--- progressh.sh
+++ progressh.sh
@@ -4,17 +4,22 @@ SPINNERS_FILE="spinners.json"
 DEFAULT_SPINNER="dots"
 
 usage() {
-  echo "Usage: $0 [--spinner NAME] [--list] [--help]"
+  echo "Usage: $0 [--spinner NAME|-s NAME] [--list|-l] [--help|-h]"
   echo
   echo "Options:"
-  echo "  --spinner NAME   Use spinner NAME from $SPINNERS_FILE (default: $DEFAULT_SPINNER)"
-  echo "  --list           List all available spinners"
-  echo "  --help           Show this help message"
+  echo "  -s, --spinner NAME   Use spinner NAME from $SPINNERS_FILE (default: $DEFAULT_SPINNER)"
+  echo "  -l, --list           List all available spinners with previews"
+  echo "  -h, --help           Show this help message"
+  echo
+  echo "Press Ctrl+C to exit the spinner."
   exit 0
 }
 
 list_spinners() {
-  jq -r 'keys[]' "$SPINNERS_FILE"
+  for name in $(jq -r 'keys[]' "$SPINNERS_FILE"); do
+    frames=$(jq -r --arg s "$name" '.[$s].frames[]' "$SPINNERS_FILE" | head -n 5 | tr '\n' ' ')
+    printf "%-15s %s...\n" "$name" "$frames"
+  done
   exit 0
 }
 
@@ -39,9 +44,9 @@ spinner="$DEFAULT_SPINNER"
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
-  --help) usage ;;
-  --list) list_spinners ;;
-  --spinner)
+  -h | --help) usage ;;
+  -l | --list) list_spinners ;;
+  -s | --spinner)
     spinner="$2"
     shift 2
     ;;
